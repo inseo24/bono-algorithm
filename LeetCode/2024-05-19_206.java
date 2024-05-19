51e9d0f231af5efdba481967277b967658d71bf8
choi seoin <befine324@gmail.com>
2024-05-19 23:50:34 +0900

    Add solution for LeetCode problem #206 in Java

---
+++ LeetCode/2024-05-19_206.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/reverse-linked-list/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
