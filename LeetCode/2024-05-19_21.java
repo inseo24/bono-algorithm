85b52a6e9a93d2c8f517757114ff45b016b08ebb
choi seoin <befine324@gmail.com>
2024-05-19 23:40:41 +0900

    Add solution for LeetCode problem #21 in Java

---
+++ LeetCode/2024-05-19_21.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// https://leetcode.com/problems/merge-two-sorted-lists/
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = list1 == null ? list2 : list1;
+        return dummy.next;
+    }
+}
